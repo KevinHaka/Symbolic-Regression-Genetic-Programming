22419790e15967b39ece52053186b5ae44ec0a13
Haka Kevin <kevihaka@gmail.com>
2025-07-18 01:01:17 +0300

    Implement SHAP feature selection class and update GP method type hints for consistency

--- src/symbolic_regression/methods/gp.py
+++ src/symbolic_regression/methods/gp.py
@@ -1,9 +1,8 @@
-from sympy import re
 from .base import BaseMethod
 from ..utils.pysr_utils import fit_and_evaluate_best_equation, nrmse_loss
 
-from pandas import DataFrame, Series
-from numpy import ndarray
+import pandas as pd
+import numpy as np
 
 from pysr import PySRRegressor
 from typing import Any, Callable, List, Tuple, Dict
@@ -11,10 +10,10 @@ from typing import Any, Callable, List, Tuple, Dict
 class GP(BaseMethod):
     def __init__(
         self,
-        loss_function: Callable[[ndarray, ndarray], float] = nrmse_loss,
+        loss_function: Callable[[np.ndarray, np.ndarray], float] = nrmse_loss,
         record_interval: int = 1,
         pysr_params: Dict[str, Any] = {}
-    ):
+    ) -> None:
         """
         Initialize the GP method with loss function, record interval, and PySR parameters.
 
@@ -27,27 +26,23 @@ class GP(BaseMethod):
         pysr_params : Dict
             Parameters for PySRRegressor.
         """
+        
         super().__init__(loss_function, record_interval, pysr_params)
-
-        # Initialize PySRRegressor with provided parameters
-        self.pysr_regressor = PySRRegressor(**self.pysr_params)
 
     def run(
         self,
         train_val_test_set: Tuple[
-            DataFrame,  # X_train
-            DataFrame,  # X_val
-            DataFrame,  # X_test
-            ndarray,    # y_train
-            ndarray,    # y_val
-            ndarray     # y_test
+            pd.DataFrame,  # X_train
+            pd.DataFrame,  # X_val
+            pd.DataFrame,  # X_test
+            np.ndarray,    # y_train
+            np.ndarray,    # y_val
+            np.ndarray     # y_test
         ],
     ) -> Tuple[
-        ndarray,               # training losses
-        ndarray,               # validation losses
-        ndarray,               # test losses
-        List[Series],          # best-equation objects
-        List[str],             # feature names
+        Tuple[np.ndarray, np.ndarray, np.ndarray],  # training, validation, test losses
+        List[pd.Series],                            # best-equation objects
+        List[str],                                  # feature names
     ]:
         """
         Execute one full train/validation/test run using GP.
@@ -62,4 +57,4 @@ class GP(BaseMethod):
             self.pysr_params
         )
 
-        return training_losses, validation_losses, test_losses, best_eqs, features
+        return (training_losses, validation_losses, test_losses), best_eqs, features
