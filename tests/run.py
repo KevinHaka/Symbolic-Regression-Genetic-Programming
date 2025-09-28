6fca941c715545f75025b47fe2ead23fc2564892
Haka Kevin <kevihaka@gmail.com>
2025-09-28 15:58:36 +0300

    Refactor run.py: streamline imports, enhance parameter handling, and improve result processing

--- tests/run.py
+++ tests/run.py
@@ -1,24 +1,12 @@
-import numpy as np
-import pickle
+import os
+from dotenv import load_dotenv, find_dotenv
+
+# Find and load .env file
+load_dotenv(find_dotenv())
+
 import datetime
 import warnings
-import time
-
-import sympy as sp
-
-# NOTE: This script assumes that the NPEET repository is cloned in the same directory as this script.
-# I should have try to fix that by creating a virtual environment and installing NPEET as a package.
-import sys
-import os
-
-# # Add the path to the cloned NPEET repository
-# script_dir = os.path.dirname(os.path.abspath(__file__))
-# npeet_path = os.path.join(script_dir, 'NPEET')
-# if os.path.isdir(npeet_path):
-#     sys.path.append(npeet_path)
-
-# # Add the project's source directory to the Python path
-# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+from numpy import arange
 
 from dask.delayed import delayed
 from dask.base import compute
@@ -29,30 +17,36 @@ from symbolic_regression.methods.gpshap import GPSHAP
 from symbolic_regression.methods.gpcmi import GPCMI
 from symbolic_regression.methods.rfgp import RFGP
 
-from symbolic_regression.utils.pysr_utils import nrmse_loss, train_val_test_split, process_task, send_email
 from symbolic_regression.datasets import load_datasets
+from symbolic_regression.utils.pysr_utils import (
+    nrmse_loss,
+    results_to_dataframe, 
+    train_val_test_split, 
+    process_task, 
+    send_email, 
+    gather_splits, 
+    extract_equations, 
+    inv, 
+    sqrt_sympy,
+    timeit,
+    load_task_results,
+    collect_results,
+    save_results
+)
 
+# Suppress specific warnings
 warnings.filterwarnings("ignore", category=RuntimeWarning)
 
-def gather_splits(split_results, index):
-    """Gathers a specific part of the train-val-test split results (e.g., index 0 for X_train)."""
-    return tuple(split[index] for split in split_results)
-
-def extract_equations(results, index):
-    """Extracts the equations from the results."""
-    return [result['equations'][index] for result in results]
-
-def inv(x):
-    return 1 / x
-
-def sqrt_sympy(x, evaluate=True):
-    return sp.sqrt(x, evaluate=evaluate)
 
 def main(): 
+    # Find the directory of the current script
     script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # create a data directory if it doesn't exist
     data_dir = os.path.join(script_dir, 'data')
     os.makedirs(data_dir, exist_ok=True)
 
+    # create a unique output directory for this run
     run_output_dir = os.path.join(data_dir, datetime.datetime.now().strftime(r"%Y%m%d%H%M%S"))
     os.makedirs(run_output_dir)
 
@@ -60,20 +54,24 @@ def main():
     log_file = os.path.join(script_dir, "logfile.log")
     if os.path.exists(log_file): os.remove(log_file)
 
-    n_runs = 10
+    # ---------------- Parameters ----------------
+
+    n_runs = 12
     test_size = 0.2
-    val_size = 0.25
+    val_size = 0.2
     n_top_features = None
     ns = 100
     ci = 0.99
     k = 5
-    record_interval = 10
+    record_interval = 5
     n_submodels = 2
+    num_workers = (os.cpu_count() or 2) // 2
+    threads_per_worker = 2
 
     pysr_params = {
-        "populations": 1,
-        "population_size": 20,
-        "niterations": 30,
+        "populations": 3,
+        "population_size": 33,
+        "niterations": 150,
         "binary_operators": ["+", "-", "*"],
         "unary_operators": ["sqrt", "inv(x) = 1/x"],
         "extra_sympy_mappings": {
@@ -86,20 +84,21 @@ def main():
         "parallelism": "serial",
         "deterministic": False,
     }
+
     dataset_names = [
         "F1",
-        "F2",
-        ("4544_GeographicalOriginalofMusic", "4544_GOM"),
+        # "F2",
+        # ("4544_GeographicalOriginalofMusic", "4544_GOM"),
         "505_tecator",
     	("Communities and Crime", "CCN"),
-        ("Communities and Crime Unnormalized", "CCUN"),
+        # ("Communities and Crime Unnormalized", "CCUN"),   
     ]
     datasets = load_datasets(dataset_names)
 
     gp_params = {
         "loss_function": nrmse_loss,
         "record_interval": record_interval,
-        **pysr_params,
+        "pysr_params": pysr_params,
     }
 
     gpshap_params = {
@@ -126,15 +125,17 @@ def main():
     methods = {
         "GP": GP(**gp_params),
         "GPSHAP": GPSHAP(**gpshap_params),
-        "GPCMI": GPCMI(**gpcmi_params),
+        # "GPCMI": GPCMI(**gpcmi_params),
     	"RFGPCMI": RFGP(**rfgpcmi_params),
     }
 
-    n_records = methods[list(methods.keys())[0]].n_records
-    delayed_tasks = {}
+    niterations = methods[next(iter(methods))].pysr_params["niterations"]
+    epochs = arange(record_interval, niterations+1, record_interval)
 
-    if "GPSHAP" in methods:
-        methods["GPSHAP"].clear_cache()  # Clear cache before starting new task
+    # ---------------- End Parameters ----------------
+
+    # n_records = methods[list(methods.keys())[0]].n_records
+    delayed_tasks = {}
 
     for dataset_name, dataset in datasets.items():
         delayed_tasks[dataset_name] = {}
@@ -198,90 +199,44 @@ def main():
         for task in task_list
     ]
     
-    # Use threaded scheduler to avoid process spawn overhead on Windows
     print("Starting computation with Dask...")  
-    # with mp.Pool(processes=os.cpu_count()) as pool:
     with ProgressBar():
         compute(
             *tasks_to_run,
             traverse=False, 
             scheduler='processes',
-            # pool=pool,  
+            num_workers=num_workers,
+            threads_per_worker=threads_per_worker 
         )
-    print("\nAll tasks completed successfully.")
+    print("All tasks completed successfully.")
 
-    # Read all saved results from individual task files
-    task_results = []
-    for filename in os.listdir(run_output_dir):
-        if filename.endswith('.pkl'):
-            filepath = os.path.join(run_output_dir, filename)
+    task_results = load_task_results(run_output_dir)
+    print(f"\nLoaded {len(task_results)} task results from {run_output_dir}")
 
-            with open(filepath, 'rb') as f:
-                result = pickle.load(f)
-                task_results.append(result)
-
-    print(f"Loaded {len(task_results)} task results from {run_output_dir}")
-
-    results = {}
-    equations = {}
-    features = {}
-
-    for dataset_name in datasets.keys():
-        results[dataset_name] = {}
-        equations[dataset_name] = {}
-        features[dataset_name] = {}
-
-        for method_name in methods.keys():
-            results[dataset_name][method_name] = {
-                "training_losses": np.empty((n_runs, n_records)),
-                "validation_losses": np.empty((n_runs, n_records)),
-                "test_losses": np.empty((n_runs, n_records)),
-            }
-            equations[dataset_name][method_name] = []
-            features[dataset_name][method_name] = []
-
-    for result in task_results:
-        dataset_name = result['dataset_name']
-        method_name = result['method_name']
-        run = result['run']
-
-        results[dataset_name][method_name]["training_losses"][run, :] = result['losses'][0]
-        results[dataset_name][method_name]["validation_losses"][run, :] = result['losses'][1]   
-        results[dataset_name][method_name]["test_losses"][run, :] = result['losses'][2]
-        
-        equations[dataset_name][method_name].append(result['equations'])
-        features[dataset_name][method_name].append(result['features'])
-
-    data = {
-        "results": results,
-        "equations": equations,
-        "features": features
-    }
-    timestamp = datetime.datetime.now().strftime(r"%Y-%m-%d__%H-%M-%S")
-    filename = f"data_{timestamp}.pickle"
-    with open(filename, "wb") as f:
-        pickle.dump(data, f)
+    results, equations, features = collect_results(task_results, datasets, methods)
+    results_df = results_to_dataframe(results, epochs)
+    filename = save_results(results_df, equations, features, prefix="data")
     print(f"\nResults saved to {filename}")
 
 if __name__ == '__main__':
-    start_time = time.time()
-    main()
-    end_time = time.time()
-
     # Calculate elapsed time
-    elapsed_time = end_time - start_time
+    elapsed_time = timeit(main)['time']
+
     # Format the elapsed time as H:MM:SS
     td_str = str(datetime.timedelta(seconds=elapsed_time))
+
     # Split to remove microseconds
     time_parts = td_str.split('.')
 
+    # Create message
     message = f"Script finished running in {time_parts[0]} seconds."
     print(message)
 
-    # Ανάγνωση credentials από μεταβλητές περιβάλλοντος
+    # Read email credentials from environment variables
     sender_email = os.getenv("EMAIL_SENDER")
     app_password = os.getenv("EMAIL_APP_PASSWORD")
 
+    # Send email notification if credentials are available
     if sender_email and app_password:
         send_email(
             subject="Script Finished Running",
@@ -293,6 +248,7 @@ if __name__ == '__main__':
     )
         
     else:
-        print("Email credentials not found in environment variables. Skipping email notification.")
+        print("Email credentials not found in environment variables.")
+        print("Skipping email notification.")
 
     
