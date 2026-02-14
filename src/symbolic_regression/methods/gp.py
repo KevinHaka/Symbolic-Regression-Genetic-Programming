25b0eabeb550d532daf3241a0a894d51de2d6e6e
Haka Kevin <kevihaka@gmail.com>
2026-02-14 23:25:26 +0200

    Refactor GP class: Update fit_and_evaluate_best_equation parameters to use n_records and events

--- src/symbolic_regression/methods/gp.py
+++ src/symbolic_regression/methods/gp.py
@@ -78,8 +78,8 @@ class GP(BaseMethod):
         training_losses, validation_losses, test_losses, best_eqs = fit_and_evaluate_best_equation(
             train_val_test_set,
             self.loss_function,
-            self.record_interval,
-            self.resplit_interval,
+            self.n_records,
+            self.events,
             self.pysr_params
         )
 
