0f74c56492ee61a8cca92dc51253688c31732e5f
Haka Kevin <kevihaka@gmail.com>
2026-04-04 23:54:31 +0300

    Refactor run.py: Improve parameter handling, enhance dataset processing, and add execution time tracking

--- examples/run.py
+++ examples/run.py
@@ -6,13 +6,14 @@ load_dotenv(find_dotenv())
 import os
 import datetime
 
+from tqdm import tqdm
 from inspect import signature
 from functools import partial
-from numpy import arange
-from numpy.random import default_rng
-from joblib import delayed
-from tqdm_joblib import ParallelPbar
 from pysr import PySRRegressor
+
+from numpy.random import default_rng
+from numpy import arange, ndarray
+from pandas import DataFrame
 
 from symbolic_regression.methods.gp import GP
 from symbolic_regression.methods.gppi import GPPI
@@ -22,8 +23,8 @@ from symbolic_regression.methods.rfgp import RFGP
 
 from symbolic_regression.utils.datasets import load_datasets
 from symbolic_regression.utils.data_utils import (
-    organize_results, results_to_dataframe, 
-    train_val_test_split, get_numpy_pandas_size
+    organize_results, get_execution_times,
+    results_to_dataframe, train_val_test_split
 )
 from symbolic_regression.utils.io_utils import load_pickle_files, persist, send_email
 from symbolic_regression.utils.system_utils import timeit, warnings_manager
@@ -40,7 +41,7 @@ def main() -> None:
     os.makedirs(data_dir, exist_ok=True)
 
     # create a unique output directory for this run
-    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    timestamp = datetime.datetime.now().strftime(r"%Y%m%d_%H%M%S")
     run_output_dir = os.path.join(data_dir, timestamp)
     run_params_dir = "_".join([run_output_dir, "params"])
 
@@ -48,9 +49,9 @@ def main() -> None:
 
     # Suppress specific warnings
     warning_filters = [
-        {"action": "ignore", "message": r"invalid value encountered in .*"},
-        {"action": "ignore", "message": r"divide by zero encountered in .*"},
-        {"action": "ignore", "message": r"overflow encountered in .*"},
+        {"action": "ignore", "message": r".*invalid value encountered.*", "category": RuntimeWarning},
+        {"action": "ignore", "message": r".*overflow encountered.*", "category": RuntimeWarning},
+        {"action": "ignore", "message": r".*divide by zero.*", "category": RuntimeWarning},
     ]
 
     # For GPSHAP
@@ -59,38 +60,37 @@ def main() -> None:
     # For GPPI
     sub_test_size = 0.3
 
-    # For GPSHAP and GPCMI
-    fs_runs = 30
+    # For GPSHAP and GPPI
+    fs_runs = 5
 
     # For GPCMI
     n_permutations = 100
     alpha = 0.01
-    k_nearest_neighbors = 3
+    k_nearest_neighbors = 5
 
     # For RFGP
-    n_submodels = 2
+    n_submodels = 3
 
     # General
     n_runs = 10
     test_size = 0.3
     val_size = 0.2
-    record_interval = 10
-    resplit_interval = 10
-    n_jobs = -1
-    random_state = 2026
+    record_interval = 5
+    resplit_interval = 5
+    random_state = None
 
     # Set random seed for reproducibility
     rng = default_rng(random_state)
 
     # Choose datasets to run
     dataset_names = [
-        "F1",
-        # "F2",
-        "Friedman1",
-        # "Friedman2",
-        # "Friedman3",
-        # "542_pollution",
-        # ("4544_GeographicalOriginalofMusic", "4544_GOM"),
+        ("F1", {"random_state": rng.integers(0, 2**32)}),
+        #("F2", {"random_state": rng.integers(0, 2**32)}),
+        ("Friedman1", {"n_samples": 1000, "noise": 0.5, "random_state": rng.integers(0, 2**32)}),
+        #("Friedman2", {"n_samples": 1000, "noise": 38, "random_state": rng.integers(0, 2**32)}),
+        #("Friedman3", {"n_samples": 1000, "noise": .032, "random_state": rng.integers(0, 2**32)}),
+        #"197_cpu_act",
+        #("4544_GeographicalOriginalofMusic", "4544_GOM"),
         # "505_tecator",
     	# ("Communities and Crime", "CCN"),
         # ("Communities and Crime Unnormalized", "CCUN"),
@@ -100,19 +100,20 @@ def main() -> None:
 
     # PySR parameters
     pysr_params = {
-        "populations": 12,
-        "population_size": 20,
-        "niterations": 50,
+        "niterations": 20,
+        "populations": 1,
+        "population_size": 25,
         "binary_operators": ["+", "-", "*"],
         "unary_operators": ["sqrt", "inv", "sin", "atan"],
 
-        "parallelism": "serial",
-        "deterministic": False if random_state is None else True,
+        "parallelism": "serial" if random_state else "multithreading",
+        "deterministic": True if random_state else False,
         "batching": True,
         "batch_size": 100,
 
         "verbosity": 0,
         "input_stream": 'devnull',
+        "temp_equation_file": True
     }
 
     # GP method parameters
@@ -153,13 +154,15 @@ def main() -> None:
     }
 
     # Define the methods to run
-    methods = {
-        "GP": GP(**gp_params),
-        "GPPI": GPPI(**gppi_params),
-        "GPSHAP": GPSHAP(**gpshap_params),
-        "GPCMI": GPCMI(**gpcmi_params),
-        "RFGPCMI": RFGP(**rfgpcmi_params),
+    methods_config = {
+        "GP": {"class": GP, "params": gp_params},
+        "GPPI": {"class": GPPI, "params": gppi_params},
+        "GPSHAP": {"class": GPSHAP, "params": gpshap_params},
+        "GPCMI": {"class": GPCMI, "params": gpcmi_params},
+        "RFGPCMI": {"class": RFGP, "params": rfgpcmi_params},
     }
+
+    # ---------------- End Parameters ----------------
 
     # Get default niterations from PySRRegressor if not provided
     niterations = pysr_params.get("niterations", signature(PySRRegressor).parameters['niterations'].default)
@@ -167,52 +170,46 @@ def main() -> None:
     # Define epochs based on record interval
     epochs = arange(record_interval, niterations+1, record_interval)
 
-    # ---------------- End Parameters ----------------
-
-    delayed_tasks = [] # To hold all delayed tasks
     splits = {} # To hold data splits for each dataset
-    exclude_split = {} # To track whether to exclude train_val_test_set from pickling
         
     # Iterate over datasets
     for dataset_name, dataset in datasets.items():
         X = dataset["X"] # Features
         y = dataset["y"] # Target
+        splits[dataset_name] = [] # Initialize splits list for this dataset
+
+        assert isinstance(X, (DataFrame)), f"X for dataset {dataset_name} must be a pandas DataFrame"
+        assert isinstance(y, (ndarray)), f"y for dataset {dataset_name} must be a numpy array"
 
         # Create splits for each run
-        splits[dataset_name] = ParallelPbar(f"Creating splits for {dataset_name}")(n_jobs=n_jobs)(
-            delayed(train_val_test_split)(X, y, test_size, val_size, rng.integers(0, 2**32))
-            for _ in range(n_runs)
-        )
-
-        # Calculate the size of the splits to determine if they should be excluded from pickling
-        split_size = sum([get_numpy_pandas_size(obj) for obj in splits[dataset_name][0]])
-        exclude_split[dataset_name] = split_size > 1024 ** 2
+        for _ in tqdm(range(n_runs), f"Creating splits for {dataset_name}", n_runs):
+            splits[dataset_name].append(train_val_test_split(X, y, test_size, val_size, rng.integers(0, 2**32)))
 
         # Iterate over methods
-        for method_name, method in methods.items():
-            # Create a delayed task for each run
-            delayed_tasks.extend(
-                delayed(persist)(
+        for method_name, method_config in methods_config.items():
+            method = method_config["class"]
+            params = method_config["params"]
+
+            # Process each run for the current method and dataset
+            for run, split in tqdm(enumerate(splits[dataset_name]), f"Processing method {method_name} for dataset {dataset_name}", n_runs):
+                persist(
                     func=partial(warnings_manager, func=process_task),
                     pickle_dir=os.path.join(run_params_dir, dataset_name, method_name),
                     filename=f"run_{run}.pkl",
                     execute=True,
                     save_result=False,
-                    exclude_keys=["train_val_test_set" if exclude_split[dataset_name] else None],
+                    exclude_keys=None,
                     filters=warning_filters,
+                    method_class=method,
+                    method_params=params,
                     dataset_name=dataset_name, 
                     method_name=method_name, 
                     run=run, 
                     train_val_test_set=split, 
-                    method=method, 
                     output_dir=run_output_dir, 
                     return_results=False,
                     random_state=rng.integers(0, 2**32)
-                ) for run, split in enumerate(splits[dataset_name])
-            )
-
-    # Execute all method tasks in parallel
-    ParallelPbar("Processing methods")(n_jobs=n_jobs)(delayed_tasks)
+                )
 
     # Load all task results from the run output directory
     task_results = load_pickle_files(run_output_dir)
@@ -222,12 +219,20 @@ def main() -> None:
     results, equations, features = organize_results(task_results)
     results_df = results_to_dataframe(results, epochs)
 
+    # Load all run parameters from the run params directory
+    run_params = load_pickle_files(run_params_dir)
+    print(f"Loaded {len(run_params)} run parameters from {run_params_dir}")
+
+    # Get execution times for each method and dataset
+    exec_times_dict = get_execution_times(run_params)
+
     # Save results to a pickle file
     filename = persist(
         filename=f"data_{timestamp}.pkl",
         df=results_df, 
         equations=equations, 
-        features=features
+        features=features,
+        execution_times=exec_times_dict,
     )
     print(f"\nResults saved to {filename}")
 
